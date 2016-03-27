ccbd7f878dd0a05f3bd304e440e1e11cbb8aeea7
Jongyeol Yang <yangjy0113@gmail.com>
2016-03-27 19:10:35 +0900

    topcoder open Round1A

---
+++ topcoder/TO2016_250.cpp
@@ -0,0 +1,209 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+
+using namespace std;
+
+class EllysTimeMachine {
+public:
+	string getTime(string);
+};
+
+string EllysTimeMachine::getTime(string time) {
+	int h, m, hh, mm;
+	h = 10*(time[0] - '0') + (time[1] - '0');
+	m = 10*(time[3] - '0') + (time[4] - '0');
+	int h2m[13] = {0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 0};
+
+	hh = m/5;
+	if (hh == 0) hh = 12;
+	mm = h2m[h];
+	char res[6];
+	sprintf(res, "%02d:%02d", hh, mm);
+	return res;
+}
+
+
+
+
+double test0() {
+	string p0 = "11:20";
+	EllysTimeMachine * obj = new EllysTimeMachine();
+	clock_t start = clock();
+	string my_answer = obj->getTime(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "04:55";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "02:25";
+	EllysTimeMachine * obj = new EllysTimeMachine();
+	clock_t start = clock();
+	string my_answer = obj->getTime(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "05:10";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "06:30";
+	EllysTimeMachine * obj = new EllysTimeMachine();
+	clock_t start = clock();
+	string my_answer = obj->getTime(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "06:30";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "05:55";
+	EllysTimeMachine * obj = new EllysTimeMachine();
+	clock_t start = clock();
+	string my_answer = obj->getTime(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "11:25";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "03:45";
+	EllysTimeMachine * obj = new EllysTimeMachine();
+	clock_t start = clock();
+	string my_answer = obj->getTime(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "09:15";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	string p0 = "01:00";
+	EllysTimeMachine * obj = new EllysTimeMachine();
+	clock_t start = clock();
+	string my_answer = obj->getTime(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "12:05";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+int main() {
+	int time;
+	bool errors = false;
+
+	time = test0();
+	if (time < 0)
+		errors = true;
+
+	time = test1();
+	if (time < 0)
+		errors = true;
+
+	time = test2();
+	if (time < 0)
+		errors = true;
+
+	time = test3();
+	if (time < 0)
+		errors = true;
+
+	time = test4();
+	if (time < 0)
+		errors = true;
+
+	time = test5();
+	if (time < 0)
+		errors = true;
+
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
